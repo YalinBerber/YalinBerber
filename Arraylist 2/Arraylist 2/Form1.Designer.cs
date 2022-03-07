86fd10deaaef2071a008c0c4babbe31131011f33
YalinBerber <100556339+YalinBerber@users.noreply.github.com>
2022-03-07 10:12:49 +0300

    Arraylist Proje 2

---
+++ Arraylist 2/Arraylist 2/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Arraylist_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDurum = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnAraEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listeSehirler = new System.Windows.Forms.ListBox();
+            this.txtSehirler = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelDurum
+            // 
+            this.labelDurum.AutoSize = true;
+            this.labelDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelDurum.Location = new System.Drawing.Point(34, 238);
+            this.labelDurum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDurum.Name = "labelDurum";
+            this.labelDurum.Size = new System.Drawing.Size(70, 25);
+            this.labelDurum.TabIndex = 0;
+            this.labelDurum.Text = "Durum";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.Blue;
+            this.btnEkle.Location = new System.Drawing.Point(12, 93);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(104, 43);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnAraEkle
+            // 
+            this.btnAraEkle.BackColor = System.Drawing.Color.Teal;
+            this.btnAraEkle.Location = new System.Drawing.Point(12, 151);
+            this.btnAraEkle.Name = "btnAraEkle";
+            this.btnAraEkle.Size = new System.Drawing.Size(104, 43);
+            this.btnAraEkle.TabIndex = 1;
+            this.btnAraEkle.Text = "ArayaEkle";
+            this.btnAraEkle.UseVisualStyleBackColor = false;
+            this.btnAraEkle.Click += new System.EventHandler(this.btnAraEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.Color.Green;
+            this.btnGuncelle.Location = new System.Drawing.Point(170, 93);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(104, 43);
+            this.btnGuncelle.TabIndex = 1;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.DarkRed;
+            this.btnSil.Location = new System.Drawing.Point(170, 151);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(104, 43);
+            this.btnSil.TabIndex = 1;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnAra
+            // 
+            this.btnAra.BackColor = System.Drawing.Color.Purple;
+            this.btnAra.Location = new System.Drawing.Point(326, 128);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(103, 43);
+            this.btnAra.TabIndex = 1;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = false;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(26, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şehirler";
+            // 
+            // listeSehirler
+            // 
+            this.listeSehirler.FormattingEnabled = true;
+            this.listeSehirler.ItemHeight = 16;
+            this.listeSehirler.Location = new System.Drawing.Point(514, 47);
+            this.listeSehirler.Name = "listeSehirler";
+            this.listeSehirler.Size = new System.Drawing.Size(175, 180);
+            this.listeSehirler.TabIndex = 2;
+            // 
+            // txtSehirler
+            // 
+            this.txtSehirler.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSehirler.Location = new System.Drawing.Point(102, 35);
+            this.txtSehirler.Name = "txtSehirler";
+            this.txtSehirler.Size = new System.Drawing.Size(100, 24);
+            this.txtSehirler.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(716, 323);
+            this.Controls.Add(this.txtSehirler);
+            this.Controls.Add(this.listeSehirler);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnAraEkle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.labelDurum);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDurum;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnAraEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listeSehirler;
+        private System.Windows.Forms.TextBox txtSehirler;
+    }
+}
+
