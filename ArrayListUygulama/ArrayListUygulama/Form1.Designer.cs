bbc552c37ebb28aeb842f6cbf5fe5526f5870ba5
YalinBerber <100556339+YalinBerber@users.noreply.github.com>
2022-03-07 10:11:49 +0300

    Arraylist Proje 1

---
+++ ArrayListUygulama/ArrayListUygulama/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ArrayListUygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbListe = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnListe = new System.Windows.Forms.Button();
+            this.txtSehirAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbListe
+            // 
+            this.lbListe.FormattingEnabled = true;
+            this.lbListe.ItemHeight = 15;
+            this.lbListe.Location = new System.Drawing.Point(330, 22);
+            this.lbListe.Name = "lbListe";
+            this.lbListe.Size = new System.Drawing.Size(145, 139);
+            this.lbListe.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.Silver;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(116, 94);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(87, 27);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnListe
+            // 
+            this.btnListe.BackColor = System.Drawing.Color.Silver;
+            this.btnListe.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListe.Location = new System.Drawing.Point(355, 177);
+            this.btnListe.Name = "btnListe";
+            this.btnListe.Size = new System.Drawing.Size(95, 34);
+            this.btnListe.TabIndex = 1;
+            this.btnListe.Text = "Listele";
+            this.btnListe.UseVisualStyleBackColor = false;
+            this.btnListe.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtSehirAdi
+            // 
+            this.txtSehirAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSehirAdi.Location = new System.Drawing.Point(102, 39);
+            this.txtSehirAdi.Name = "txtSehirAdi";
+            this.txtSehirAdi.Size = new System.Drawing.Size(116, 23);
+            this.txtSehirAdi.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sehir Adı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(518, 223);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSehirAdi);
+            this.Controls.Add(this.btnListe);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lbListe);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbListe;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnListe;
+        private System.Windows.Forms.TextBox txtSehirAdi;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
