a518a6fa4fa14ab9b34e85da88405c46d2e8ceff
YalinBerber <100556339+YalinBerber@users.noreply.github.com>
2022-03-07 10:15:13 +0300

    Arraylist Proje 5

---
+++ ArraylistProje5/ArraylistProje5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArraylistProje5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Stack yigin = new Stack();
+ 
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            yigin.Push(txtAdSoyad.Text);
+            Listele();
+        }
+        private void Listele()
+        {
+            listeStack.Items.Clear();
+            foreach (string eleman in yigin)
+            {
+                listeStack.Items.Add(eleman);
+            }
+        }
+
+        private void btnCikart_Click(object sender, EventArgs e)
+        {
+            yigin.Pop();
+            Listele();
+        }
+    }
+}
